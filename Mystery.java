1e0ed64ffc99cdcf724e5f613677474c89b2fe26
bouhajja ala <bouhajjaala@gmail.com>
2020-10-14 15:02:39 +0100

    Adding prints

--- Mystery.java
+++ Mystery.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Mystery {
     public static void main (String args[]){
-        
+        System.out.println("Bienvenue dans la jeu"); 
+        System.out.println("Choisir le niveau"); 
+        System.out.println("1) Niveau facile"); 
+        System.out.println("2) Niveau moyen"); 
+        System.out.println("3) Niveau difficile"); 
+        System.out.println("0) Quitter"); 
     }
 }
